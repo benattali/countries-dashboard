20e64967fb0f45d21e9295f4190a90e72fe0b0d4
Ben Attali <ben.attali8@gmail.com>
2025-10-22 18:58:07 +0300

    stick footer to bottom

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-brandBlue text-center m-4">
-        Welcome to the worlds countries dashboard
-      </h1>
-      <main>
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <h1 className="text-3xl font-bold text-brandBlue text-center m-4">
+          Welcome to the world's countries dashboard
+        </h1>
+      </header>
+
+      <main className="flex-grow">
         <Dashboard />
       </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
